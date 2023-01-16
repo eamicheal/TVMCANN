585e762b670ed29e83d340fc7344aa8819062acb
Aniebiet Micheal Ezekiel <90110919+eamicheal@users.noreply.github.com>
2023-01-16 17:50:38 +0100

    Optimized_old drop_layers

---
+++ TUTORIAL/skip1.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  7 00:03:50 2022
+
+@author: libo
+"""
+
+import numpy as np
+import torch
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from torch import nn
+import time
+
+T1 = time.perf_counter()
+
+file = '10persons.xlsx'
+df = pd.read_excel(file)
+
+df = df.fillna(0)
+df = df.iloc[:, 1:46]
+y = df.iloc[:, -1]
+X = df.iloc[:, :-1]
+X = np.asarray(X).astype(np.float32)
+X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.2)
+
+
+class MyMode(nn.Module):
+    def __init__(self):
+        super(MyMode,self).__init__() 
+        self.LayerX1 = nn.Sequential(
+            nn.Linear(in_features=44, out_features=24),
+            nn.ReLU())
+        self.LayerX2 = nn.Sequential(
+            nn.Linear(in_features=24, out_features=10),
+            nn.ReLU())
+        self.LayerX3 = nn.Sequential(            
+                # nn.Dropout(),
+                nn.Linear(in_features=10, out_features=1),
+                nn.Sigmoid(),
+            )
+        self.bypass_x1 = False
+        self.bypass_x2 = False
+        self.bypass_x3 = False
+
+        
+    def forward(self, x):
+        if self.bypass_x1:
+            self.x1 = x
+        else:
+            self.x1 = self.LayerX1(x)
+            
+        if self.bypass_x2:
+            self.x2 = x
+        else:
+            self.x2 = self.LayerX2(self.x1)
+            
+        if self.bypass_x3:
+            self.x3 = x
+        else:
+            self.x3 = self.LayerX3(self.x2)
+
+        return self.x3
+
+
+mod = torch.load("MyMod.pth")
+
+print(mod(torch.tensor( X_test[1]))) #orignal 
+
+mod.bypass_x1 = True
+loadx1 = np.load("x1.npy")
+
+print(mod(torch.tensor(loadx1))) #load
+
+T2 =time.perf_counter()
+print('runtime: %s ms' % ((T2 - T1)*1000))
